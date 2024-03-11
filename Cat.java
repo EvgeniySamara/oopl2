adb1dd202c39b99e001d96da0d958177b425b795
Evgeniy <Jack-inc@yandex.ru>
2024-03-11 20:21:04 +0400

    source

---
+++ Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    private String name;
+    private int appetite;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat() { }
+}
